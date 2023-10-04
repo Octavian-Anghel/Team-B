1df8dada7869aed053bb352b4cb55b437abc3af5
gabe-russell <gabriel.russell@maine.edu>
2023-10-04 12:25:07 -0400

    created tab buttons for 3 languages

--- devhelper/src/devhelper.tsx
+++ devhelper/src/devhelper.tsx
@@ -7,9 +7,11 @@ function App() {
     <header className="App-header">
         DevHelper
       </header>
-      <p>
-          PlaceHolder
-        </p>
+      <div className="App-tabs">
+    <button className="App-tab-button active" id="tab1-btn">Python</button>
+    <button className="App-tab-button" id="tab2-btn">Java</button>
+    <button className="App-tab-button" id="tab3-btn">C Programming</button>
+      </div>
     </div>
   );
 }
