3caf3fc3e1a1009e9464ed0824361bcf85484128
gabe-russell <gabriel.russell@maine.edu>
2023-10-22 12:46:55 -0400

    added axios api and created hompage

--- devhelper/src/devhelper.tsx
+++ devhelper/src/devhelper.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import './App.css';
+import Homepage from './components/homepage';
 
 function App() {
   return (
     <div className="App">
-    <header className="App-header">
-        DevHelper
-      </header>
-      <div className="App-tabs">
-        <button className="App-tab-button" id="tab1">Python</button>
-        <button className="App-tab-button" id="tab2">Java</button>
-        <button className="App-tab-button" id="tab3">C Programming</button>
-      </div>
-      <p> under construction</p>
+      <Homepage />
     </div>
   );
 }
