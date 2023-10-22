965e3babd5830d857d5ea13a2398725368a53623
gabe-russell <gabriel.russell@maine.edu>
2023-10-22 13:26:42 -0400

    edit register.tsx

--- devhelper/src/components/Register.tsx
+++ devhelper/src/components/Register.tsx
@@ -1,46 +1,55 @@
-// Registration.js
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom'; // Import the useHistory hook
+import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 
 const Registration = () => {
-  const history = useHistory(); // Initialize the history object using useHistory()
+  const history = useHistory();
 
+  const [name, setName] = useState(''); // New state for the name field
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
   const handleRegistration = async () => {
     try {
       const response = await axios.post('YOUR_API_REGISTER_ENDPOINT', {
+        name,  // Send the name field as well to the API
         username,
         password,
       });
 
       if (response.status === 201) {
-        // Registration successful, you may want to save a token or user data
-        history.push('/homepage'); // Redirect to the homepage after successful registration
+        history.push('/homepage');
       }
     } catch (error) {
       console.error('Registration failed:', error);
-      // Handle registration error here
     }
   };
 
   return (
     <div>
       <h2>Register</h2>
+
+      <input  // New input field for the name
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
       <input
         type="text"
         placeholder="Username"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
       />
+
       <input
         type="password"
         placeholder="Password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
+
       <button onClick={handleRegistration}>Register</button>
     </div>
   );
--- devhelper/src/devhelper.tsx
+++ devhelper/src/devhelper.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './components/homepage';
 import LoginPage from './components/Login';
+import SignUp from './components/Register';
 
 const App: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const App: React.FC = () => {
       <Switch>
         <Route exact path="/" component={LoginPage} />
         <Route path="/homepage" component={HomePage} />
+        <Route path="/signup" component={SignUp}/>
       </Switch>
     </Router>
   );
