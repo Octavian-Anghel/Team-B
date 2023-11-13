9cc8c8b9cb6001146ddb04a376e96aff459fef82
Gabriel Fitzpatrick <gabriel.fitzpatrick@maine.edu>
2023-11-12 23:42:18 -0500

    Added Functionality to hompage.tsx: Added a button to return to login. Added functionality to Login.tsx: Added a registration button to go to the register page, added functionality to redirect users on succesful login. Added funcitonality to Register.tsx: Added functionality to registration email confirmation.

--- devhelper/src/components/Register.tsx
+++ devhelper/src/components/Register.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
 import { firebaseConfig } from "../firebase-config";
+import { useHistory, Link } from 'react-router-dom';
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -11,6 +12,7 @@ const Register: React.FC = () => {
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
     const [message, setMessage] = useState<string>('');
+    const history = useHistory();  // Use the useHistory hook to get the history object
 
     const handleSubmit = async () => {
         if (email && password) {
@@ -26,23 +28,38 @@ const Register: React.FC = () => {
         }
     };
 
+    const handleReturnToLogin = () => {
+        // Delay the page refresh by 500 milliseconds
+        setTimeout(() => {
+            // Redirect to login page after the delay
+            history.push('/');
+            // Refresh the page after the redirect
+            window.location.reload();
+        }, 500);
+    };
+
     return (
         <div className="registration-container">
             <h2>Registration</h2>
             <div>
                 <label>
-                    Email: 
+                    Email:
                     <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
                 </label>
             </div>
             <div>
                 <label>
-                    Password: 
+                    Password:
                     <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                 </label>
             </div>
             <button onClick={handleSubmit}>Register</button>
             {message && <p>{message}</p>}
+
+            {/* Button to navigate back to the login page with refresh functionality */}
+            <Link to="/" onClick={handleReturnToLogin}>
+                <button className="App-button">Return to Login</button>
+            </Link>
         </div>
     );
 };
