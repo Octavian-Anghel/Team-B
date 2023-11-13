f5334d62ab5e8cb250e15e34ad06389ec3c8163c
Gabriel Fitzpatrick <gabriel.fitzpatrick@maine.edu>
2023-11-12 23:50:46 -0500

    Fixed Registration Email Not sending

--- devhelper/src/components/Register.tsx
+++ devhelper/src/components/Register.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import { getAuth, createUserWithEmailAndPassword, sendEmailVerification } from 'firebase/auth';
 import { firebaseConfig } from "../firebase-config";
 import { useHistory, Link } from 'react-router-dom';
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getFirestore(app);
+const auth = getAuth(app);
 
 const Register: React.FC = () => {
     const [email, setEmail] = useState<string>('');
@@ -17,8 +19,15 @@ const Register: React.FC = () => {
     const handleSubmit = async () => {
         if (email && password) {
             try {
-                await addDoc(collection(database, "User Data"), { email, password });
-                setMessage('Registration successful!');
+                const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+
+                // Send email verification
+                await sendEmailVerification(userCredential.user);
+
+                // Save user data to the database
+                await addDoc(collection(database, "User Data"), { email, uid: userCredential.user.uid });
+
+                setMessage('Registration successful! Please check your email for verification.');
             } catch (error) {
                 console.error("Error adding user: ", error);
                 setMessage('Registration failed. Please try again.');
