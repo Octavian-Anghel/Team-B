261e5b5aa5b166499b6237deb99fe930da782c78
gabe-russell <gabriel.russell@maine.edu>
2023-12-04 11:40:36 -0500

    editing message unit tests

--- devhelper/src/automated tests/message.test.tsx
+++ devhelper/src/automated tests/message.test.tsx
@@ -1,21 +1,16 @@
-import { MessageComponent } from '../components/Message';
-
-// Mock the Firebase Firestore functions
-jest.mock('firebase/firestore', () => ({
-  getFirestore: jest.fn(),
-  collection: jest.fn(),
-  getDocs: jest.fn(),
-  query: jest.fn(),
-  where: jest.fn(),
-}));
-
+import { MessageComponent } from '../components/Message'; 
 describe('MessageComponent', () => {
-  let messageComponent: MessageComponent;
+    const messageComponent = new MessageComponent();
 
-  describe('getMotivationalMessage', () => {
-    it('should return a motivational message', () => {
-      const motivationalMessage = messageComponent.getMotivationalMessage();
-      expect(messageComponent['motivationalQuotes']).toContain(motivationalMessage);
+    test('getMotivationalMessage returns a string', () => {
+        const message = messageComponent.getMotivationalMessage();
+        expect(typeof message).toBe('string');
     });
-  });
+
+    test('getMotivationalMessage returns random messages', () => {
+        const message1 = messageComponent.getMotivationalMessage();
+        const message2 = messageComponent.getMotivationalMessage();
+        // Note: This test might occasionally fail due to the randomness, so it's not entirely reliable
+        expect(message1).not.toBe(message2);
+    });
 });
