c6da490064bce68071f8a2f373fb98e6aeaeaa10
Octavian Anghel <octavian.anghel@maine.edu>
2023-12-04 20:08:12 -0500

    implement useState/handler functions

--- devhelper/src/components/forum/ForumFrontEnd.tsx
+++ devhelper/src/components/forum/ForumFrontEnd.tsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import ViewPost from "./ViewPost";
 import WritePost from "./WritePost";
+import { Post } from "./Types";
 
 const ForumFrontEnd = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [view, setView] = useState<"VIEW" | "WRITE">("VIEW");
+
+  const handlePostSubmitted = (newPost: Post) => {
+    setPosts((currentPosts) => [...currentPosts, newPost]);
+    setView("VIEW"); // Switch back to viewing posts
+  };
+
+  const handleWritePostClick = () => {
+    setView("WRITE"); // Switch to writing a new post
+  };
+
   return (
     <div>
       <h1>Forum</h1>
-      <ViewPost />
-      <button>Create New Post</button>
+      {view === "VIEW" && (
+        <>
+          {posts.map((post) => (
+            <ViewPost
+              key={post.id}
+              post={post}
+              onWritePostClick={handleWritePostClick}
+            />
+          ))}
+          <button onClick={handleWritePostClick}>Create New Post</button>
+        </>
+      )}
+      {view === "WRITE" && <WritePost onPostSubmitted={handlePostSubmitted} />}
     </div>
   );
 };
