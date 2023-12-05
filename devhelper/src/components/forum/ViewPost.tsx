125d76e69fb8e583ed4d91d2aada1b8a6112360c
Octavian Anghel <octavian.anghel@maine.edu>
2023-12-04 20:12:05 -0500

    "Refactor types and simplify ViewPost logic"

--- devhelper/src/components/forum/ViewPost.tsx
+++ devhelper/src/components/forum/ViewPost.tsx
@@ -1,43 +1,13 @@
-import React, { useState, useEffect } from "react";
-
-// Define the structure of the props and state
-interface Post {
-  id: number;
-  title: string;
-  content: string;
-  userName: string;
-  votes: number;
-  comments: Comment[];
-}
-
-interface Comment {
-  id: number;
-  content: string;
-}
+import React from "react";
+import { Post } from "./Types";
 
 interface ViewPostProps {
   post: Post;
   onWritePostClick: () => void;
 }
 
-const ViewPost = ({ post, onWritePostClick }: ViewPostProps) => {
-  const [comments, setComments] = useState<Comment[]>(post.comments || []);
-  const [newComment, setNewComment] = useState("");
-
-  const handleNewCommentChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNewComment(e.target.value);
-  };
-
-  const handleCommentSubmit = () => {
-    // Assuming you want to add a new comment to the local state
-    const updatedComments = [
-      ...comments,
-      { id: Date.now(), content: newComment },
-    ];
-    setComments(updatedComments);
-    setNewComment("");
-  };
-
+const ViewPost: React.FC<ViewPostProps> = ({ post, onWritePostClick }) => {
+  // Render the post and its comments
   return (
     <div className="post-container">
       <div className="post-header">
@@ -48,24 +18,14 @@ const ViewPost = ({ post, onWritePostClick }: ViewPostProps) => {
       </div>
       <div className="post-content">{post.content}</div>
       <div className="post-actions">
-        <button>Upvote</button>
-        <button>Downvote</button>
+        {/* Post actions like upvote and downvote would go here */}
       </div>
       <div className="comments-section">
-        {comments.map((comment) => (
+        {post.comments.map((comment) => (
           <div key={comment.id}>{comment.content}</div>
         ))}
       </div>
-      <div className="post-comment-box">
-        <input
-          type="text"
-          placeholder="Write a comment..."
-          value={newComment}
-          onChange={handleNewCommentChange}
-        />
-        <button onClick={handleCommentSubmit}>Submit</button>
-      </div>
-      <button onClick={onWritePostClick}>Write Post</button>
+      <button onClick={onWritePostClick}>Reply</button>
     </div>
   );
 };
