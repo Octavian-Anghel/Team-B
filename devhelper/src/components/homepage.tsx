a660946a4f48d27ee1a20cc2675108a541c18a7d
Octavian Anghel <octavian.anghel@maine.edu>
2023-12-06 14:10:26 -0500

    link forum to homepage

--- devhelper/src/components/homepage.tsx
+++ devhelper/src/components/homepage.tsx
@@ -1,39 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import { MessageComponent } from './Message';
-import '../App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import ForumFrontEnd from "./forum/ForumFrontEnd";
+import { MessageComponent } from "./Message";
+import "../App.css";
 
 function Homepage() {
-  const history = useHistory();
-  const [entryMessage, setEntryMessage] = useState('');
+  const [entryMessage, setEntryMessage] = useState("");
 
   useEffect(() => {
     const messageComponent = new MessageComponent();
     setEntryMessage(messageComponent.getMotivationalMessage());
-  }, []);  
-
-  const handleRefresh = () => {
-    setTimeout(() => {
-      history.push('/');
-      window.location.reload();
-    }, 500);
-  };
+  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        DevHelper
-      </header>
-      {entryMessage && <div className="App-message">{entryMessage}</div>}
-      <div className="App-tabs">
-        <button className="App-tab-button" id="tab1">Python</button>
-        <button className="App-tab-button" id="tab2">Java</button>
-        <button className="App-tab-button" id="tab3">C Programming</button>
-      </div>
-      <p> under construction</p>
-      
-      <button className="App-button" onClick={handleRefresh}>Back to Login</button>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/forum">
+          <ForumFrontEnd />
+        </Route>
+        <Route exact path="/">
+          <div className="App">
+            <header className="App-header">DevHelper</header>
+            {entryMessage && <div className="App-message">{entryMessage}</div>}
+            <div className="App-tabs">
+              <button className="App-tab-button" id="tab1">
+                Python
+              </button>
+              <button className="App-tab-button" id="tab2">
+                Java
+              </button>
+              <button className="App-tab-button" id="tab3">
+                C Programming
+              </button>
+            </div>
+            <p> under construction</p>
+
+            {/* Link to the forum */}
+            <Link to="/forum" className="App-button">
+              Go to Forum
+            </Link>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
