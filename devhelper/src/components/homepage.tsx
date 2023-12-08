c5c4809265b236e4e686c5e004387c0d94e38840
gabe-russell <gabriel.russell@maine.edu>
2023-12-08 17:19:42 -0500

    fixing website functionality

--- devhelper/src/components/homepage.tsx
+++ devhelper/src/components/homepage.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import '../App.css';
 import { MessageComponent } from './Message';
 
 const Homepage: React.FC = () => {
   const history = useHistory();
+  const [entryMessage, setEntryMessage] = useState('');
 
   const navigate = (path: string) => {
     history.push(path);
@@ -21,6 +22,30 @@ const Homepage: React.FC = () => {
       window.location.reload();
     }, 500);
   };
+  const handlePythonRefresh = () => {
+    setTimeout(() => {
+      history.push('/python');
+      window.location.reload();
+    }, 500);
+  };
+  const handleJavaRefresh = () => {
+    setTimeout(() => {
+      history.push('/java');
+      window.location.reload();
+    }, 500);
+  };
+  const handleCRefresh = () => {
+    setTimeout(() => {
+      history.push('/c');
+      window.location.reload();
+    }, 500);
+  };
+  const handleForumRefresh = () => {
+    setTimeout(() => {
+      history.push('/forum');
+      window.location.reload();
+    }, 500);
+  };
 
   return (
     <div className="App">
@@ -29,11 +54,12 @@ const Homepage: React.FC = () => {
       </header>
       {entryMessage && <div className="App-message">{entryMessage}</div>}
       <div className="App-tabs">
-        <button className="App-tab-button" onClick={() => navigate('/python')}>Python</button>
-        <button className="App-tab-button" onClick={() => navigate('/java')}>Java</button>
-        <button className="App-tab-button" onClick={() => navigate('/c-programming')}>C Programming</button>
+        <button className="App-tab-button" onClick={handlePythonRefresh}>Python</button>
+        <button className="App-tab-button" onClick={handleJavaRefresh}>Java</button>
+        <button className="App-tab-button" onClick={handleCRefresh}>C</button>
+        <button className="App-tab-button" onClick={handleForumRefresh}>Forum</button>
       </div>
-      <p> under construction</p>
+      <p> Welcome to DevHelper, the go-to platform for programming enthusiasts and developers alike! Whether you're interested in Python, Java, or C Programming, DevHelper provides a streamlined and intuitive experience.licking on any language tab takes you to a dedicated page, enriched with resources and documentation specific to that language. Moreover, our platform encourages continuous learning and goal setting, allowing users to define and track their learning objectives, whether short-term or long-term. DevHelper stands out with its focus on usability and resource accessibility, making it an invaluable tool for both beginners and experienced developers in their coding journey. With DevHelper, every user is one click away from a vast repository of knowledge and a supportive community, making the journey into the world of programming more engaging and efficient. </p>
       <button className="App-button" onClick={handleRefresh}>Back to Login</button>
     </div>
   );
