c8655810b5fa0a9b47a403f553dbd08fcce86bfa
Octavian Anghel <octavian.anghel@maine.edu>
2023-12-09 17:40:15 -0500

    Add upvote/downvote features, alter tests

--- devhelper/src/components/forum/ForumFrontEnd.tsx
+++ devhelper/src/components/forum/ForumFrontEnd.tsx
@@ -2,52 +2,51 @@ import React, { useState } from "react";
 import ViewPost from "./ViewPost";
 import WritePost from "./WritePost";
 import { Post } from "./Types";
-import { useHistory } from "react-router-dom";
 
 const ForumFrontEnd = () => {
   const [posts, setPosts] = useState<Post[]>([]);
   const [view, setView] = useState<"VIEW" | "WRITE">("VIEW");
-  const history = useHistory();
 
   const handlePostSubmitted = (newPost: Post) => {
     setPosts((currentPosts) => [...currentPosts, newPost]);
-    setView("VIEW"); // Switch back to viewing posts
+    setView("VIEW");
   };
 
-  const handleWritePostClick = () => {
-    setView("WRITE"); // Switch to writing a new post
+  const handleUpvote = (postId: number) => {
+    setPosts((currentPosts) =>
+      currentPosts.map((post) =>
+        post.id === postId ? { ...post, votes: post.votes + 1 } : post
+      )
+    );
   };
-  const handleRefresh = () => {
-    setTimeout(() => {
-      history.push('/homepage');
-      window.location.reload();
-    }, 500);
+
+  const handleDownvote = (postId: number) => {
+    setPosts((currentPosts) =>
+      currentPosts.map((post) =>
+        post.id === postId ? { ...post, votes: post.votes - 1 } : post
+      )
+    );
   };
 
   return (
     <div>
-      <div>
-        <h1>Forum</h1>
-        {view === "VIEW" && (
-          <>
-            {posts.map((post) => (
-              <ViewPost
-                key={post.id}
-                post={post}
-                onWritePostClick={handleWritePostClick}
-              />
-            ))}
-            <button onClick={handleWritePostClick}>Create New Post</button>
-          </>
-        )}
-        {view === "WRITE" && <WritePost onPostSubmitted={handlePostSubmitted} />}
-      </div>
-      <div>
-        <button className="App-button" onClick={handleRefresh}>Back to Homepage</button>
-      </div>
+      <h1>Forum</h1>
+      {view === "VIEW" && (
+        <>
+          {posts.map((post) => (
+            <ViewPost
+              key={post.id}
+              post={post}
+              onUpvote={() => handleUpvote(post.id)}
+              onDownvote={() => handleDownvote(post.id)}
+            />
+          ))}
+          <button onClick={() => setView("WRITE")}>Create New Post</button>
+        </>
+      )}
+      {view === "WRITE" && <WritePost onPostSubmitted={handlePostSubmitted} />}
     </div>
   );
-  
 };
 
 export default ForumFrontEnd;
--- devhelper/src/components/forum/ViewPost.tsx
+++ devhelper/src/components/forum/ViewPost.tsx
@@ -1,39 +1,62 @@
-// Assume that ViewPost.tsx is in the same directory
-
-// ViewPost.tsx
 import React from "react";
 import { Post } from "./Types";
 
 interface ViewPostProps {
   post: Post;
-  onWritePostClick: () => void;
+  onUpvote: () => void;
+  onDownvote: () => void;
+  onWritePostClick?: () => void;
 }
 
-const ViewPost: React.FC<ViewPostProps> = ({ post, onWritePostClick }) => {
+const ViewPost: React.FC<ViewPostProps> = ({ post, onUpvote, onDownvote }) => {
   return (
     <div className="post-container">
-      <div className="post-header">
-        <div className="user-info">{/* User info here */}</div>
-        {/* Post Title Box */}
-        <div className="post-title-box">
-          <h1>{post.title}</h1>
-        </div>
+      <div className="post-title-box">
+        <h1>{post.title}</h1>
+        <p>{post.content}</p>
+        <div className="post-votes">Votes: {post.votes}</div>
       </div>
-      {/* Post Content */}
-      <div className="post-content-box">{post.content}</div>
-      {/* Post Actions */}
-      <div className="post-actions">{/* Voting actions */}</div>
-      {/* Comments Section */}
+      <div className="post-actions">
+        <button onClick={onUpvote} aria-label="upvote">
+          {/* Upvote SVG */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-arrow-up"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5"
+            />
+          </svg>
+        </button>
+        <button onClick={onDownvote} aria-label="downvote">
+          {/* Downvote SVG */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-arrow-down"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1"
+            />
+          </svg>
+        </button>
+      </div>
       <div className="comments-section">
-        {post.comments.map((comment) => (
-          // Comment Box
-          <div key={comment.id} className="comment-box">
-            {comment.content}
+        {post.comments.map((comment, index) => (
+          <div key={index} className="comment-box">
+            <p>{comment.content}</p>
           </div>
         ))}
       </div>
-      {/* Write Post/Reply Button */}
-      <button onClick={onWritePostClick}>Reply</button>
     </div>
   );
 };
